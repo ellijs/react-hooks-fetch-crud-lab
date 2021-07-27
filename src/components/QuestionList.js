41701410673684885b48b6eaea057558dd74a47d
ellijs <ellijs@hotmail.com>
2021-07-27 02:37:53 -0400

    almost

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({ questions, handleDeleteEvent, handleUpdateEvent }) {
+
+  const renderQuestions = questions.map(question => <QuestionItem key={question.id} question={question} handleDeleteEvent={handleDeleteEvent} handleUpdateEvent={handleUpdateEvent}/>)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{renderQuestions}</ul>
     </section>
   );
 }
